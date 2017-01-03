36c38873cefb3e4188ef2a57d03efebf347a1049
springxiege@sina.cn <heiyu@shelpe.com>
2017-01-03 15:23:26 +0800

    完成alert方法

--- src/modalBox/js/modalBox.js
+++ src/modalBox/js/modalBox.js
@@ -1,38 +1,59 @@
 /**
  * modalBox 主要文件
  */
-var modalBox = modalBox || function() {
+var modalBox = modalBox || (function() {
     // 保存已创建对话框的引用
     var dialogs = {};
-    /**
-     * [extend 扩展属性]
-     * @method extend
-     * @param  {[type]} base [初始属性]
-     * @param  {[type]} sub  [扩展属性]
-     * @return {[type]}      [Object]
-     */
-    var extend = function(base, sub){
-        for (var prop in sub) {
-            if (base.hasOwnProperty(prop)) {
-                base[prop] = sub[prop];
-            }
+    dialogs.mask = {
+        show: function(options){
+            var defaults = {};
+            var opts = $.extend(true, {}, defaults, options || {});
+            var $mask = $('<div class="modal-mask" />');
+            $mask.css(opts);
+            $('body').append($mask);
+        },
+        hide: function($dialogs){
+            $dialogs.prev('.modal-mask').remove();
         }
-        return base;
     };
-    /**
-     * [register description]
-     * @param  {[type]} name    [description]
-     * @param  {[type]} Factory [description]
-     * @return {[type]}         [description]
-     */
-    var register = function(name, Factory) {
+    dialogs.alert = function(msg,options){
+        var _this = this;
         var defaults = {
-            dialog: null,
-            factory: Factory
+            css: {},
+            manul:true,
+            iconClass: null,
+            okbtn: function(){},
+            cancelbtn: function(){}
         };
-        return dialogs[name] = defaults;
-    };
-    dialogs.alert = function(){
+        var opts = $.extend(true, {}, defaults, options || {});
+        var $alert = $('<div class="modal-alert"><span class="alert-close j-close">&times;</span><div class="alert-content"></div></div>');
+        $alert.close = function(){
+            _this.mask.hide($alert);
+            $alert.remove();
+        };
+        if(!$.isEmptyObject(opts.css)){
+            $alert.css(opts.css);
+        }
+        if(!!opts.okbtn){
+            $alert.append('<p><span class="btn btn-primary alert-ok j-okbtn">确定</span>');
+        }
+        if(!!opts.iconClass){
+            $alert.find('.alert-content').before('<h6><i class="' + opts.iconClass + '"></i></h6>');
+        }
+        $alert.find('.alert-content').html(msg)
+            .end()
+            .on('click','.j-close',function(){
+                _this.mask.hide($alert);
+                $alert.remove();
+                opts.okbtn && opts.cancelbtn($alert);
+            })
+            .on('click','.j-okbtn',function(){
+                !opts.manul && _this.mask.hide($alert);
+                !opts.manul && $alert.remove();
+                opts.okbtn && opts.okbtn($alert);
+            })
+        _this.mask.show();
+        $('body').append($alert);
 
     };
     dialogs.confirm = function(){
@@ -42,4 +63,4 @@ var modalBox = modalBox || function() {
         return {};
     };
     return dialogs;
-};
+})();
